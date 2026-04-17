b3405b364f4845e82567532426108f879058ee24
mahansh-headout <mahansh.aditya@headout.com>
2026-04-17 19:25:05 +0530

    Fix: Prisma Migration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import {ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-    [x: string]: any;
-    constructor(config: ConfigService){
-        super({
-            datasources: {
-                db: {
-                    url: config.get<string>('DATABASE_URL')
-                }
-            }
-        })
-    }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(private readonly config: ConfigService) {
+    const connectionString = config.getOrThrow("DATABASE_URL")
+    const adapter = new PrismaPg({ connectionString })
+
+    super({ adapter })
+  }
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
 }
+
